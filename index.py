a50426c81024ce2107be5c3ed76d7a15b0ef24d0
Aviral Agarwal <aviralagarwal25@gmail.com>
2020-10-07 21:03:16 +0530

    Add function to merge intermidiate indices

--- index.py
+++ index.py
@@ -3,27 +3,69 @@
 
     * Assigns a docID to each document in the corpus.
     * Parses The documents to create intermidiate inverted indices.
-    * Uses Block Sort and Merge Algorithm to generate the unified inverted index.
+    * Uses Block Sort and Merge Algorithm to generate an unified inverted index.
 
 """
 
 import pickle
 import os
+import copy
 from settings import INDEX_SIZE
 
 
-def open_file(curr_file_no):
-    """
-    Opens a new pickle file
-
-       * accepts an integer - current file number as operand
-       * returns the file object for newly opened file
-
-    """
-
-    filename = "./index_files/temp_index" + str(curr_file_no) + ".pkl"
+# Helper functions for the Block Sort Based Indexing Algorithm
+def open_file(curr_file_no=None, filename=None):
+    if curr_file_no:
+        filename = "./index_files/temp_index" + str(curr_file_no) + ".pkl"
     file_obj = open(filename, "wb")
     return file_obj
+
+
+def sort_list(unsorted_list):
+    return sorted(unsorted_list, key=lambda x: (x[0], x[1]))
+
+
+def write_to_file(index_obj, curr_list):
+    for entry in curr_list:
+        pickle.dump(entry, index_obj, pickle.HIGHEST_PROTOCOL)
+
+
+def read_object(ptrs, ptr):
+    try:
+        return pickle.load(ptr)
+    except EOFError:
+        ptrs.remove(ptr)
+        return None
+
+
+def find_smallest(obj_list):
+    smallest = ()
+    small_ptr = None
+    for obj, ptr in obj_list:
+        if not smallest or obj[0] < smallest[0]:
+            smallest = obj
+            small_ptr = ptr
+        elif obj[0] == smallest[0] and obj[1] < smallest[1]:
+            smallest = obj
+            small_ptr = ptr
+    obj_list.remove((smallest, small_ptr))
+    try:
+        new_obj = pickle.load(small_ptr)
+        obj_list.append((new_obj, small_ptr))
+    except EOFError:
+        small_ptr.close()
+    return (smallest, obj_list)
+
+
+def assign_Id():
+    """Assigns an id numbber to each document in the corpus"""
+    id_dict = {}
+    curr_id = 1
+    for filename in os.listdir("corpus"):
+        id_dict[curr_id] = filename
+        curr_id += 1
+    with open("./index_files/docId.pkl", "wb") as f:
+        pickle.dump(id_dict, f, pickle.HIGHEST_PROTOCOL)
 
 
 def parse_docs():
@@ -40,7 +82,6 @@ def parse_docs():
     with open("./index_files/docId.pkl", "rb") as f:
         id_dict = pickle.load(f)
         curr_file_no = 1
-        index_obj = open_file(curr_file_no)
         curr_list = []
 
         # TODO: Normalization of documents
@@ -52,39 +93,54 @@ def parse_docs():
                         curr_list.append((word.lower(), id))
                         if len(curr_list) >= INDEX_SIZE:
                             curr_list = sort_list(curr_list)
+                            index_obj = open_file(curr_file_no=curr_file_no)
                             write_to_file(index_obj, curr_list)
                             curr_list = []
                             index_obj.close()
                             curr_file_no += 1
-                            index_obj = open_file(curr_file_no)
+        if curr_list:
+            curr_list = sort_list(curr_list)
+            index_obj = open_file(curr_file_no=curr_file_no)
+            write_to_file(index_obj, curr_list)
+            index_obj.close()
 
 
-def write_to_file(index_obj, curr_list):
-    for entry in curr_list:
-        pickle.dump(entry, index_obj, pickle.HIGHEST_PROTOCOL)
-
-
-def assign_Id():
-    id_dict = {}
-    curr_id = 1
-    for filename in os.listdir("corpus"):
-        id_dict[curr_id] = filename
-        curr_id += 1
-    with open("./index_files/docId.pkl", "wb") as f:
-        pickle.dump(id_dict, f, pickle.HIGHEST_PROTOCOL)
-
-
-def sort_list(unsorted_list):
-    return sorted(unsorted_list, key=lambda x: (x[0], x[1]))
-
-
-# temporary function to print pickle files. To be removed later.
-def display():
+def merge_indices():
     file_list = os.listdir("index_files")
+    index_obj = open_file(filename="./index_files/index.pkl")
+    ptrs = []
+    curr_list = []
+    obj_list = []
     for filename in file_list:
-        with open("./index_files/" + filename, "rb") as openfile:
-            while True:
-                try:
-                    print(pickle.load(openfile))
-                except EOFError:
-                    break
+        if filename != "docId.pkl" and filename != "index.pkl":
+            ptrs.append(open("./index_files/" + filename, "rb"))
+    for ptr in ptrs:
+        print(ptr.name)
+        obj = pickle.load(ptr)
+        obj_list.append((obj, ptr))
+    while obj_list:
+        req_obj, obj_list = find_smallest(obj_list)
+        curr_list.append(req_obj)
+        if len(curr_list) > INDEX_SIZE:
+            write_to_file(index_obj, curr_list)
+            curr_list = []
+    if curr_list:
+        write_to_file(index_obj, curr_list)
+    index_obj.close()
+
+
+# Temporary function to print pickle files. To be removed later.
+def display():
+    with open("./index_files/index.pkl", "rb") as openfile:
+        while True:
+            try:
+                print(pickle.load(openfile))
+            except EOFError:
+                break
+
+
+if __name__ == "__main__":
+    assign_Id()
+    parse_docs()
+    merge_indices()
+    display()
