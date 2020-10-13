95419bc891239866a57aa51b41929c1af1058812
Aviral Agarwal <aviralagarwal25@gmail.com>
2020-10-13 22:59:48 +0530

    Fix error for query with no valid word.

--- tf_idf_calculation.py
+++ tf_idf_calculation.py
@@ -13,7 +13,8 @@ def calculate_term_tf_idf(term):
     document_frequency = len(term_frequencies.keys())
 
     # use (total_number_of_docs + 1) as numerator to handle cases when document_frequency = total_number_of_docs
-    idf = log10(total_number_of_docs + 1 / document_frequency)
+    # use (1 + document_frequency) as denominator to handle cases when document_frequency comes out to be zero
+    idf = log10(total_number_of_docs + 1 / 1 + document_frequency)
     tf_idf_weights = []
     for document_data in term_frequencies.items():
         docId, freq = document_data
@@ -43,3 +44,6 @@ def calculate_query_tf_idf(query):
         tf_idf_weights.append((docId, tf_idf))
 
     return tf_idf_weights
+
+
+print(calculate_query_tf_idf(["and"]))
