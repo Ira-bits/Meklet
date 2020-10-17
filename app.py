210dba522ee730bd7edfce57f90e7b737f5b9733
Aviral Agarwal <aviralagarwal25@gmail.com>
2020-10-17 23:11:46 +0530

    Create basic flask app

---
+++ app.py
@@ -0,0 +1,61 @@
+import flask
+from flask import request, jsonify, make_response
+import search_engine
+
+# Initialize FLask app
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True  # Change to False in Production
+
+
+def regular_search(query):
+    processed_query = search_engine.process_string(query)
+    return sorted(
+        search_engine.calculate_query_tf_idf(processed_query), key=lambda x: x[1]
+    )
+
+
+@app.route("/", methods=["GET"])
+def home():
+    return "Serve Home Page from this URL!"
+
+
+@app.route("/api/search-results", methods=["GET"])
+def api_search():
+    params = request.args
+    advanced = params["advanced"]
+    query = params["query"]
+
+    # Validate Request Parameters
+    try:
+        assert type(advanced) == str
+        assert type(query) == str
+        if advanced not in ["true", "false"]:
+            raise AssertionError()
+    except AssertionError:
+        response = make_response("Invalid Request Parameters", 400)
+        return response
+
+    if advanced == "true":
+        pass  # TODO: Advanced Search
+    else:
+        results = regular_search(query)
+
+    # Convert the list of results to JSON format.
+    return jsonify(results)
+
+
+if __name__ == "__main__":
+
+    # Download Required Dependencies
+    search_engine.download_nltk_deps()
+
+    print("Do you want to recreate the index? (y/n)")
+    recreate = False if input().lower() == "n" else True
+
+    if recreate:
+        print("Constructing Index ...")
+        search_engine.start_indexing()
+        print("Index Successfully Created!")
+
+    # Start the Server process
+    app.run(use_reloader=False)
