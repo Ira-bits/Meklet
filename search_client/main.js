85c3beff88120093514979f340adc07dafd7aabb
Akul Singhal <42439193+predator810@users.noreply.github.com>
2020-10-18 12:12:28 +0530

    fix cors and complete basic search client

--- search_client/main.js
+++ search_client/main.js
@@ -1,21 +1,62 @@
 "use strict";
-document.getElementsByClassName("search-button")[0].onclick = () => {
-    let searchQuery = {
-        queryText: document.getElementsByClassName("search-input")[0].value,
+let serverUrl = "http://localhost:5000";
+
+let handleSearch = (e) => {
+    e.preventDefault();
+    let searchParams = {
+        advanced: "false",
+        query: document.getElementsByClassName("search-input")[0].value,
     };
-    // fetch(url, {
-    //     body: searchQuery,
-    // }).then((res) => {
-    //     console.log(res);
-    //     if (res.ok) {
-    //         let changeInitStyle = Array.from(
-    //             document.getElementsByClassName("init")
-    //         );
-    //         console.log(changeInitStyle);
-    //         for (let element of changeInitStyle) {
-    //             console.log(element);
-    //             element.classList.remove("init");
-    //         }
-    //     }
-    // });
+    let url = new URL(serverUrl + "/api/search-results");
+    url.search = new URLSearchParams(searchParams).toString();
+    fetch(url)
+        .then((res) => {
+            if (res.ok) {
+                // Change search bar position
+                let changeInitStyle = Array.from(
+                    document.getElementsByClassName("init")
+                );
+                for (let element of changeInitStyle) {
+                    element.classList.remove("init");
+                }
+            }
+            return res.json();
+        })
+        .then((data) => {
+            // Clear previous results
+            document.getElementsByClassName(
+                "search-results-container"
+            )[0].innerHTML = "";
+            if (data.length == 0) {
+                document.getElementsByClassName(
+                    "search-results-container"
+                )[0].innerHTML = "<h2>No Results</h2>";
+            } else {
+                data.forEach((result) => {
+                    let resultItem = document.createElement("div");
+                    resultItem.className = "result-item";
+                    // Use data from result once API is complete
+                    let title = "Temp Result";
+                    let resultLink = "./";
+                    let summary = result[1].toString();
+                    resultItem.innerHTML =
+                        '<h3><a href ="' +
+                        resultLink +
+                        '">' +
+                        title +
+                        "</a></h3>" +
+                        "<p>" +
+                        summary +
+                        "</p>" +
+                        "<hr/>";
+                    document
+                        .getElementsByClassName("search-results-container")[0]
+                        .appendChild(resultItem);
+                });
+            }
+        });
 };
+
+document
+    .getElementsByClassName("search-form")[0]
+    .addEventListener("submit", handleSearch);
