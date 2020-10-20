1bddc5e9aaf3b9cf2f19e100f4e96f932be2390d
HarshvardhanJha1 <46719471+HarshvardhanJha1@users.noreply.github.com>
2020-10-21 01:36:09 +0530

    Implement LRU cache and time metrics (#16)
    * Implement LRU cache and time metrics
    
    * Increase size of Cache

--- helper.py
+++ helper.py
@@ -4,6 +4,37 @@ import search_engine
 import pickle
 import sys
 from pathlib import Path
+from collections import OrderedDict
+
+class LRUCache:
+ 
+    # initialising capacity
+    def __init__(self, capacity: int):
+        self.cache = OrderedDict()
+        self.capacity = capacity
+ 
+    # we return the value of the key
+    # that is queried in O(1) and return -1 if we
+    # don't find the key in out dict / cache.
+    # And also move the key to the end
+    # to show that it was recently used.
+    def get(self, key: int) -> int:
+        if key not in self.cache:
+            return -1
+        else:
+            self.cache.move_to_end(key)
+            return self.cache[key]
+ 
+    # first, we add / update the key by conventional methods.
+    # And also move the key to the end to show that it was recently used.
+    # But here we will also check whether the length of our
+    # ordered dictionary has exceeded our capacity,
+    # If so we remove the first key (least recently used)
+    def put(self, key: int, value: int) -> None:
+        self.cache[key] = value
+        self.cache.move_to_end(key)
+        if len(self.cache) > self.capacity:
+            self.cache.popitem(last = False)
 
 
 def get_link_title_for_docId(docId, id_dict):
@@ -19,11 +50,12 @@ def get_link_title_for_docId(docId, id_dict):
 def regular_search(query):
     """ Takes in a query and returns a list of corresponding (docId,freq) pairs """
     processed_query = search_engine.process_string(query)
-    return sorted(
+    res = sorted(
         search_engine.calculate_query_tf_idf(processed_query),
         key=lambda x: x[1],
         reverse=True,
     )[:10]
+    return res
 
 
 def merge(list_a, list_b, operator="and"):
@@ -100,4 +132,5 @@ def advanced_search(query):
         result.sort(key=lambda x: x[0])
         final_result = merge(final_result, result, operator="or")
 
-    return sorted(final_result, key=lambda x: x[1], reverse=True)[:10]
+    res= sorted(final_result, key=lambda x: x[1], reverse=True)[:10]
+    return res
